b8183814ce0e9a8de4c7153727fa40c11cecdfc9
Le QuangDung <quangdung100194@gmail.com>
2018-10-18 18:26:04 +0700

    test re-commit

--- pages/homePage/index.jsx
+++ pages/homePage/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FormattedMessage, injectIntl } from "react-intl";
 import { loadFakeDataUsers, loadFakeDataPosts } from "./action";
