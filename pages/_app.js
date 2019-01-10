1000d61319add3173af74d448062930547a0121e
didilinkin <929213769@qq.com>
2019-01-10 18:24:24 +0800

    Add actionDemo.js

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,7 @@ import { IntlProvider, addLocaleData } from "react-intl";
 import locale_en from "react-intl/locale-data/en";
 import locale_ja from "react-intl/locale-data/ja";
 import locale_vi from "react-intl/locale-data/vi";
+
 import messages_ja from "../translations/ja.json";
 import messages_en from "../translations/en.json";
 import messages_vi from "../translations/vi.json";
